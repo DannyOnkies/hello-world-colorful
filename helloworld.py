ef40a9eee83a7d988f8d5802d45a2f64b284a6f2
Danny <80686975+DannyOnkies@users.noreply.github.com>
2021-04-23 14:17:23 +0200

    Add files via upload
    The classic HELLOWORLD , a little more complex

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# My first program
+from random import randint
+
+greetings = ['Hello World' , 'Hola Mundo' , 'Ciao Mondo' , 'Bonjour le Monde', 'Hallo Welt']
+value = randint(0,4)
+line='-'
+greeting = greetings[value]
+print(line.strip() * len(greeting))
+print(greeting)
+print(line.strip() * len(greeting))
