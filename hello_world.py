81ad48b16404f8e71af53ebad39184431646272c
Danny <80686975+DannyOnkies@users.noreply.github.com>
2021-05-02 19:46:39 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,12 +2,22 @@
 A random HELLOWORLD
 """
 
+from colorama import *
 from random import randint
 
-greetings = ['Hello World' , 'Hola Mundo' , 'Ciao Mondo' , 'Bonjour le Monde', 'Hallo Welt']
+# colorama initialise
+init()
+
+greetings = ['Hello World    .' , 'Hola Mundo     .' , 'Ciao Mondo     .' , 'Bonjour le Monde', 'Hallo Welt     .']
 value = randint(0,4)
 line='-'
 greeting = greetings[value]
-print(line.strip() * len(greeting))
-print(greeting)
-print(line.strip() * len(greeting))
+linex = line.strip() * len(greeting)
+
+print(Cursor.POS(5,5)+ linex)
+print(Fore.BLUE +
+          Back.LIGHTYELLOW_EX +
+          Cursor.POS(5, 6) + # (col,row)
+          greeting +
+          Style.RESET_ALL)
+print(Cursor.POS(5,7)+linex)
